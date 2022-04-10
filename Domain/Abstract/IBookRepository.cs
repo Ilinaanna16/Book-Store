b4daba50735aab0224b87bb27be7d797ca2fef1d
Ilinaanna16 <100427245+Ilinaanna16@users.noreply.github.com>
2022-04-10 16:42:25 +0300

    Add files via upload

--- Domain/Abstract/IBookRepository.cs
+++ Domain/Abstract/IBookRepository.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
-using Domain.Entities;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +10,7 @@ namespace Domain.Abstract
     public interface IBookRepository
     {
         IEnumerable<Book> Books { get; }
+        void SaveBook(Book book);
+        Book DeleteBook(int bookId);
     }
 }
--- Domain/Concrete/EFDbContext.cs
+++ Domain/Concrete/EFDbContext.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Domain.Concrete
 {
     public class EFDbContext : DbContext
     {
-        public DbSet <Book> Books { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
